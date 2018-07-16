4072dc76475698fb4fc55667494d9b646a8f7782
NaderAbdelrahman <nader.abdelrahman@tapad.com>
2018-07-16 15:11:17 -0400

    m

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 fs.readFile('README.md', 'utf8', (err, fileContent) => {
     if (err) throw err;
 
+    const [ metadata, content ] = fileContent.split(`${ METADATA_SEPARATOR }\n`);
+    // console.log(metadata);
+
     fs.writeFile('README.html', md.render(content), (err) => {
         if (err) throw err;
         // console.log(./README.html);

06a1c35c2c4cc2be95d7985990c8eb8cf87486e2
NaderAbdelrahman <nader.abdelrahman@tapad.com>
2018-07-17 15:05:50 -0400

    m

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const md = require('markdown-it')();
 const fs = require('fs');
 
-fs.readFile('README.md', 'utf8', (err, fileContent) => {
+fs.readFile('*.md', 'utf8', (err, fileContent) => {
 
     if (err) throw err;
 
-    fs.writeFile('README.html', md.render(fileContent), (err) => {
+    fs.writeFile('*.html', md.render(fileContent), (err) => {
         if (err) throw err;
     })
 
